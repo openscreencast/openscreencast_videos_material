bd6b132925fd024c286f30ed5308e77cae0b4737
openscreencast <info@openscreencast.de>
2017-03-12 18:00:28 +0100

    add Python fuer Anfaenger - ein Rechteck zeichnen

---
+++ openscreencast_videos_python_fuer_anfaenger/python_beginners_ein_rechteck_zeichnen/python_beginners_ein_rechteck_zeichnen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# python_beginners_ein_rechteck_zeichnen.py
+# Lizenz: https://creativecommons.org/publicdomain/zero/1.0/deed.de
+
+import turtle
+
+turtle.forward(25)
+turtle.left(30)
+turtle.reset()
+turtle.shape("turtle")
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+help(turtle.color)
+
+turtle.color('red')
+turtle.forward(100)
+turtle.undo()
+ 
+
