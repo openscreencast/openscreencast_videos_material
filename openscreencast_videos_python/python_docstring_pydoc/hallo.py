e1f2c8dc54d96f4a0b40335bbb0140371f1fd48e
openscreencast <info@openscreencast.de>
2017-03-12 17:47:27 +0100

    add Python - docstring und pydoc

---
+++ openscreencast_videos_python/python_docstring_pydoc/hallo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# hallo.py
+# Lizenz: https://creativecommons.org/publicdomain/zero/1.0/deed.de
+
+def hallo():
+    """Funktion hallo() - Ausgabe von Hallo !
+
+    Beispiel: hallo()
+    """
+    print("Hallo !")
+
+def hi():
+    """Funktion hi() - Ausgabe von Hi !
+
+    Beispiel: hi()
+    """
+    print("Hi !")
+
+hallo()
+hi()
+
+print(hi.__doc__)
+print(hallo.__doc__)
+
+
